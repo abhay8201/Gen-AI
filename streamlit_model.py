373b003f2da311cd72b89e167b3eb3eb3dc09ddf
Abhay Rana <123184188+abhay8201@users.noreply.github.com>
2025-02-11 09:32:43 +0530

    Add files via upload

---
+++ streamlit_model.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from langchain_groq import ChatGroq
+
+llm = ChatGroq(
+    model = "mixtral-8x7b-32768",
+    temperature=0,
+    groq_api_key="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"
+)
+
+st.title("simple llm chatBot")
+st.write("Enter your query below:")
+user_input = st.text_input("Your Question:","")
+
+if st.button("Get Answer"):
+    response = llm.invoke(user_input)
+    st.write("### Response:")
+    st.write(response.content)
